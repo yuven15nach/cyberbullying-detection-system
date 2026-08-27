c06a46d89f79a754a01a19540cf18d06b2990c43
Yuven Nach <yuven15nach@gmail.com>
2026-08-27 12:47:52 +0800

    commit

--- utils/gemini_api.py
+++ utils/gemini_api.py
@@ -1,7 +1,20 @@
+import os
 import streamlit as st
 import google.generativeai as genai
+from dotenv import load_dotenv
 
-api_key = st.secrets["GEMINI_API_KEY"]
+# Load local .env
+load_dotenv()
+
+# Get API key from local .env first
+api_key = os.getenv("GEMINI_API_KEY")
+
+# If not available, get it from Streamlit Secrets
+if not api_key:
+    api_key = st.secrets.get("GEMINI_API_KEY")
+
+if not api_key:
+    raise ValueError("GEMINI_API_KEY is not configured.")
 
 genai.configure(api_key=api_key)
 
